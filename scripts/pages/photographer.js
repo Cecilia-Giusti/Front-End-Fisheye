547538551166493d65ef6cb32a55678efcb71a9c
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-03-24 10:23:09 +0100

    Feat: add photographerPage for presentation of photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Fonction pour récuperer les données du json
+async function getPhotographersHeader() {
+  return fetch("../data/photographers.json")
+    .then((file) => file.json())
+    .then((data) => data.photographers);
+}
+
+//Fonction pour créer la section de présentation du photographe
+async function displayDataHeader(photographers) {
+  const photographersHeaderPage = document.querySelector(".photograph-header");
+
+  photographers.forEach((photographer) => {
+    const template = new PhotographerHeaderPage(photographer);
+    photographersHeaderPage.appendChild(
+      template.createPhotographerHeaderPage()
+    );
+  });
+}
+
+//Fonction d'initiation de la page d'accueil
+async function init() {
+  const photographers = await getPhotographersHeader();
+  displayDataHeader(photographers);
+}
+
+init();
--- scripts/templates/photographerCard.js
+++ scripts/templates/photographerCard.js
@@ -1,4 +1,4 @@
-// Classe pour créer une carte - Template pattern
+// Classe pour créer une carte
 class PhotographerCard {
   constructor(photographer) {
     this._photographer = photographer;
@@ -10,15 +10,15 @@ class PhotographerCard {
     const picture = `assets/photographers/${this._photographer.portrait}`;
 
     const photographerCard = `
-            <div class="img-content">
-                <img src="${picture}" alt="Photo portrait de ${this._photographer.name}"/>
-            </div>
-            <div class="photographer-content">
-            <h1>${this._photographer.name}</h1>
-            <h2>${this._photographer.city}, ${this._photographer.country}</h2>
-            <p class="description"> ${this._photographer.tagline}</p>
-            <p class="price"> ${this._photographer.price}€/jour</p>
-            </div>
+            <a href="./photographer.html">
+              <img class="img_photographer img_photographerIndex--size" src="${picture}" alt="Photo portrait de ${this._photographer.name}"/>
+              <div class="photographer-content">
+                <h2>${this._photographer.name}</h2>
+                <h3>${this._photographer.city}, ${this._photographer.country}</h3>
+                <p class="description"> ${this._photographer.tagline}</p>
+                <p class="price"> ${this._photographer.price}€/jour</p>
+              </div>
+            </a>
         `;
 
     $wrapper.innerHTML = photographerCard;
