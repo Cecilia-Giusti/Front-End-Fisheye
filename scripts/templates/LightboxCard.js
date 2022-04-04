27765fffbd0574e1e6bfd48e94161f54983e0bc2
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-04-04 15:18:55 +0200

    Style: update css

--- scripts/templates/LightboxCard.js
+++ scripts/templates/LightboxCard.js
@@ -7,13 +7,13 @@ class LightboxCard {
 
   createLightboxCard() {
     const lightBoxcard = `
-    <button class="btn previous"><i class="fa-solid fa-angle-left"></i></button>
+    <button class="btn previous"><i class="fi fi-bs-angle-left"></i></button>
     <div class="lightbox_content--img">
         ${this._media.media}
         <h4> ${this._media.title}</h4>
         </div>
-        <button class="btn next"><i class="fa-solid fa-angle-right"></i></button>
-        <button class="btn close"><i class="fa-solid fa-xmark"></i></button>
+        <button class="btn next"><i class="fi fi-bs-angle-right"></i></button>
+        <button class="btn close"><i class="fi fi-bs-cross"></i></button>
         `;
     this.$wrapper.setAttribute("class", "lightbox_content");
     this.$wrapper.innerHTML = lightBoxcard;
