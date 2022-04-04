3521a3bd812662adcd7c8d342c5decf7fbdc8801
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-04-04 15:44:54 +0200

    Fix: try change links

--- scripts/Api/fetch.js
+++ scripts/Api/fetch.js
@@ -1,13 +1,13 @@
 // Fonction pour récuperer les données du json
 // eslint-disable-next-line no-unused-vars
 async function getPhotographers() {
-  return fetch("../data/photographers.json")
+  return fetch("./data/photographers.json")
     .then((file) => file.json())
     .then((data) => data.photographers);
 }
 // eslint-disable-next-line no-unused-vars
 async function getMedias() {
-  return fetch("../data/photographers.json")
+  return fetch("./data/photographers.json")
     .then((file) => file.json())
     .then((data) => data.media);
 }
