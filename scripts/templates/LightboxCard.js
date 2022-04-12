e6fc1bcffe3293cec0a9ca60e6f0a6bf076b7fbe
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-04-12 12:32:25 +0200

    Style : update contact form accessibility

--- scripts/templates/LightboxCard.js
+++ scripts/templates/LightboxCard.js
@@ -13,14 +13,16 @@ class LightboxCard {
         ${this._media.media}
         <h4 tabindex="0"> ${this._media.title}</h4>
       </div>
-      <button class="btn previous"><i class="fi fi-bs-angle-left"></i></button>
-      <button class="btn next"><i class="fi fi-bs-angle-right"></i></button>
-      <button class="btn close"><i class="fi fi-bs-cross"></i></button>
+      <button aria-label="Previous image" class="btn previous"><i class="fi fi-bs-angle-left"></i></button>
+      <button aria-label="Next image" class="btn next"><i class="fi fi-bs-angle-right"></i></button>
+      <button aria-label="Close Dialog" class="btn close"><i class="fi fi-bs-cross"></i></button>
         `;
     this.$wrapper.setAttribute("class", "lightbox_content");
+    this.$wrapper.setAttribute("role", "document");
     this.$wrapper.innerHTML = lightBoxcard;
 
     this.$wrapper.querySelector(".lightbox_content--img img");
+
     return this.$wrapper;
   }
 }
--- scripts/utils/Lightbox.js
+++ scripts/utils/Lightbox.js
@@ -57,8 +57,9 @@ class Lightbox {
       this.close();
     });
 
+    // Gestion des flèches du clavier
     document
-      .querySelector(".lightbox_content--img")
+      .querySelector(".lightbox_content")
       .addEventListener("keyup", (e) => {
         console.log(e.key);
         switch (e.key) {
@@ -87,18 +88,48 @@ class Lightbox {
     const sectionLightbox = document.querySelector("#lightbox");
     sectionLightbox.setAttribute("class", "display-flex");
     sectionLightbox.appendChild(lightbox.createLightboxCard());
+
+    // Gestion des évenements
     this.manageEvent();
 
+    // Gestion de l'accessibilité
+    document.querySelector(".lightbox_content").setAttribute("tabindex", "-1");
+    document.querySelector(".lightbox_content").setAttribute("role", "dialog");
+    document
+      .querySelector(".lightbox_content")
+      .setAttribute("aria-label", "image closeup view");
+    document.querySelector(".lightbox_content").focus();
+
+    document.querySelector(".lightbox_content--img img").removeAttribute("alt");
+
+    document
+      .querySelector(".lightbox_content--img img")
+      .setAttribute("alt", `${this._currentElement._title}`);
+
+    //Gestion des médias videos
     if (document.querySelector(".lightbox_content--img video")) {
       document
         .querySelector(".lightbox_content--img video")
         .setAttribute("controls", "");
     }
+
+    //Focus pour l'accessibilité
+    sectionLightbox.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "Tab":
+          document.querySelector(".lightbox_content--img").focus();
+          break;
+        case "Escape":
+          this.close();
+          break;
+      }
+    });
   }
   // Méthodes pour la fermeture de la lightbox
   close() {
     this.remove();
     document.querySelector("#lightbox").setAttribute("class", "display-none");
+    this.$wrapper.focus();
   }
 
   remove() {
